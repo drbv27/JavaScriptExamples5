788a2b901b4c3c8271509785a385f4e52ab6e3a4
Diego Bonilla <drbv27@gmail.com>
2021-05-11 14:28:05 -0500

    Initial Commit

---
+++ assets/app.js
@@ -0,0 +1,30 @@
+/*Ejercicios Basicos JS Nivel 5
+    15.Programa una función para convertir números de base binaria a decimal y viceversa.Ejemplo: miFuncion(100,2) devolverá 4 base 10.
+    16.Programa una función que devuelva el monto final después de aplicar un descuento a una cantidad dada.Ejemplo: miFuncion(1000, 20) devolverá 800.
+    17.Programa una función que dada una fecha válida determine cuantos años han pasado hasta el día de hoy.Ejemplo: miFuncion(new Date(1984,4,23)) devolverá 35 años (en 2020).
+*/
+
+//Soluciones:
+
+//--------------------------------------------------------------------------------------------
+
+//E15S1.Solucion Simple:
+console.log("-----------------------------------------------------------------------------------")
+console.log("Ejercicio 15:")
+
+
+
+//--------------------------------------------------------------------------------------------
+
+//E16S1.Solución Simple
+console.log("-----------------------------------------------------------------------------------")
+console.log("Ejercicio 16:")
+
+
+
+//--------------------------------------------------------------------------------------------
+
+//E17S1.Solución Simple
+console.log("-----------------------------------------------------------------------------------")
+console.log("Ejercicio 17:")
+
